1558396801e1b55a9e8a7dfa78009d4cfc849de0
StudentOfJS <rodlewis45@gmail.com>
2023-01-14 08:27:21 +0800

    1 file modified

--- src/createGoogleSpeechToText.ts
+++ src/createGoogleSpeechToText.ts
@@ -1,6 +1,6 @@
-import { google } from 'googleapis';
+import { google, speech_v1 } from 'googleapis';
 
-const createGoogleSpeechToText = async (): Promise<google.speech.v1.Speech> => {
+const createGoogleSpeechToText = async (): Promise<speech_v1.Speech> => {
   const auth = await google.auth.getClient({
     keyFile: 'path/to/apiKey.json',
     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
