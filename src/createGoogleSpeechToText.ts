96e42606002ddb4758832d1f980c104a0445b60f
StudentOfJS <rodlewis45@gmail.com>
2023-01-14 08:46:54 +0800

    2 files modified

--- src/createGoogleSpeechToText.ts
+++ src/createGoogleSpeechToText.ts
@@ -1,12 +1,15 @@
 import { google, speech_v1 } from 'googleapis';
 
-const createGoogleSpeechToText = async (): Promise<speech_v1.Speech> => {
+export const createGoogleSpeechToText = async (): Promise<speech_v1.Speech> => {
   const auth = await google.auth.getClient({
     keyFile: 'path/to/apiKey.json',
     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
   });
-
+  // global
+  google.options({
+    // All requests made with this object will use these settings unless overridden.
+    timeout: 1000,
+    auth: auth,
+  });
   return google.speech({ version: 'v1', auth });
 };
-
-export { createGoogleSpeechToText };
