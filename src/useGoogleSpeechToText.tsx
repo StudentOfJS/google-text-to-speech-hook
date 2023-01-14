61c09c1c82facd6fedb997d7ebb9bef5c2965ff3
StudentOfJS <rodlewis45@gmail.com>
2023-01-14 08:14:47 +0800

    3 files modified

--- src/useGoogleSpeechToText.tsx
+++ src/useGoogleSpeechToText.tsx
@@ -1,53 +1,56 @@
-import { useState, useEffect } from 'react';
 import { google } from 'googleapis';
+import { useState, useEffect } from 'react';
 import { createGoogleSpeechToText } from './createGoogleSpeechToText';
 
 interface ITranscription {
-    transcription: string;
-    error: Error | null;
-    isListening: boolean;
-    startListening: () => void;
-    stopListening: () => void;
+  transcription: string;
+  error: Error | null;
+  isListening: boolean;
+  startListening: () => void;
+  stopListening: () => void;
 }
 
 export const useGoogleSpeechToText = (): ITranscription => {
-    const [transcription, setTranscription] = useState('');
-    const [error, setError] = useState<Error | null>(null);
-    const [isListening, setIsListening] = useState(false);
+  const [transcription, setTranscription] = useState('');
+  const [error, setError] = useState<Error | null>(null);
+  const [isListening, setIsListening] = useState(false);
 
-    let speechToText:google.speech.v1.Speech;
-    let listener: google.Listener;
+  let speechToText: google.speech.v1.Speech;
+  let listener: { stop: () => void; };
 
-    const startListening = async () => {
-        try {
-            speechToText = await createGoogleSpeechToText();
-            listener = speechToText.listen((transcription) => {
-                setTranscription(transcription);
-            }, {
-                interimResults: true
-            });
-            setIsListening(true);
-        } catch (err) {
-            setError(err);
+  const startListening = async () => {
+    try {
+      speechToText = await createGoogleSpeechToText();
+      listener = speechToText.listen(
+        (transcription: string) => {
+          setTranscription(transcription);
+        },
+        {
+          interimResults: true,
         }
+      );
+      setIsListening(true);
+    } catch (err) {
+      setError(err as Error);
+    }
+  };
+
+  const stopListening = async () => {
+    try {
+      listener.stop();
+      setIsListening(false);
+    } catch (err) {
+      setError(err as Error);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (listener) {
+        listener.stop();
+      }
     };
+  }, []);
 
-    const stopListening = async () => {
-        try {
-            listener.stop();
-            setIsListening(false);
-        } catch (err) {
-            setError(err);
-        }
-    };
-
-    useEffect(() => {
-        return () => {
-            if (listener) {
-                listener.stop();
-            }
-        };
-    }, []);
-
-    return { transcription, error, isListening, startListening, stopListening };
+  return { transcription, error, isListening, startListening, stopListening };
 };
